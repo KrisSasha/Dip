ed16398cb1180c5e803282153e02b52b68b7f774
Kessy <sanya.zzkd@gmail.com>
2023-07-24 16:40:27 +0300

    Переименован столбец выборки для 2 запроса

--- Запросы.sql
+++ Запросы.sql
@@ -5,7 +5,7 @@ JOIN "Orders" AS o ON c.id = o."courierId"
 WHERE o."inDelivery" = true 
 GROUP BY c.login;
 
-SELECT tracker, 
+SELECT track, 
 CASE
 WHEN finished = true THEN 2
 WHEN cancelled = true THEN -1
